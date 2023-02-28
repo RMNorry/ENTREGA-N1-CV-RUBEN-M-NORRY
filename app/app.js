771c0badb5a9d2aaf841e536e4d9c78e85d72fe0
RMNorry <126542518+RMNorry@users.noreply.github.com>
2023-02-28 01:44:26 -0300

    Primera subida
    Primera subida

---
+++ app/app.js
@@ -0,0 +1,50 @@
+
+
+const foto = document.querySelector('#foto')
+const nombre = document.querySelector('#nombre')
+const apellido = document.querySelector('#apellido')
+const correo = document.querySelector('#correo')
+const telefono = document.querySelector('#telefono')
+const pais = document.querySelector('#pais')
+const edad = document.querySelector('#edad')
+const provincia = document.querySelector('#provincia')
+
+
+
+
+
+async function generarUsuario() {
+   try {
+      const url = 'https://randomuser.me/api/'
+      const respuesta = await fetch(url)
+      const { results } = await respuesta.json()
+      const datos = results[0]
+
+      console.log(datos)
+      foto.src = datos.picture.large
+      nombre.textContent = datos.name.first
+      apellido.textContent = datos.name.last
+      correo.textContent = datos.email
+      telefono.textContent = datos.phone
+      pais.textContent = datos.location.country
+      provincia.textContent = datos.location.state
+    
+      edad.textContent = datos.dob.age
+   
+   } catch (error) {
+      console.log(error)
+   }
+
+}
+document.addEventListener('DOMContentLoaded',generarUsuario)
+
+   
+
+function toggleInfo() {
+   var info = document.getElementById("info");
+   if (info.style.display === "none") {
+     info.style.display = "block";
+   } else {
+     info.style.display = "none";
+   }
+ }
